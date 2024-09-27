725f5032de9caf73b80de33360c98f10b8cfe278
Gastón Frenkel <gst.frenkel@gmail.com>
2024-09-27 18:24:21 -0300

    queue binding

--- pkg/broker/amqpconn/amqp.go
+++ pkg/broker/amqpconn/amqp.go
@@ -33,12 +33,45 @@ func (b *messageBroker) QueueDeclare(name string) (amqp.Queue, error) {
 	return b.ch.QueueDeclare(name, true, false, false, false, nil)
 }
 
+func (b *messageBroker) QueuesDeclare(names ...string) ([]amqp.Queue, error) {
+	var queues []amqp.Queue
+
+	for _, n := range names {
+		q, err := b.ch.QueueDeclare(n, true, false, false, false, nil)
+		if err != nil {
+			return nil, err
+		} else {
+			queues = append(queues, q)
+		}
+	}
+
+	return queues, nil
+}
+
 func (b *messageBroker) ExchangeDeclare(name string, kind string) error {
 	return b.ch.ExchangeDeclare(name, kind, true, false, false, false, nil)
 }
 
+func (b *messageBroker) ExchangesDeclare(exchanges ...broker.Exchange) error {
+	for _, ex := range exchanges {
+		if err := b.ExchangeDeclare(ex.Name, ex.Kind); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (b *messageBroker) QueueBind(name string, key string, exchange string) error {
 	return b.ch.QueueBind(name, key, exchange, false, nil)
+}
+
+func (b *messageBroker) QueuesBind(binds ...broker.QueueBind) error {
+	for _, bind := range binds {
+		if err := b.ch.QueueBind(bind.Name, bind.Key, bind.Exchange, false, nil); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
 func (b *messageBroker) ExchangeBind(dst string, key string, src string) error {
--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -2,10 +2,24 @@ package broker
 
 import amqp "github.com/rabbitmq/amqp091-go"
 
+type Exchange struct {
+	Name string
+	Kind string
+}
+
+type QueueBind struct {
+	Name     string
+	Key      string
+	Exchange string
+}
+
 type MessageBroker interface {
 	QueueDeclare(name string) (amqp.Queue, error)
+	QueuesDeclare(name ...string) ([]amqp.Queue, error)
 	ExchangeDeclare(name string, kind string) error
+	ExchangesDeclare(exchange ...Exchange) error
 	QueueBind(name string, key string, exchange string) error
+	QueuesBind(binds ...QueueBind) error
 	ExchangeBind(dst string, key string, src string) error
 	Publish(exchange string, key string, msg amqp.Publishing) error
 	Close()
