1ba64672b6f376ea504f80e6e1d0e381d898e71b
Lucas <lgrati@fi.uba.ar>
2024-09-28 17:12:26 -0300

    client scaffolding

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,21 @@
 package client
+
+import (
+	"log"
+)
+
+type Client struct {
+	// TODO
+}
+
+// NewClient creates a new Client instance with the provided configuration parameters.
+func NewClient() *Client {
+	return &Client{}
+}
+
+// Start launches the client and begins processing queues and files.
+func (c *Client) Start() {
+	log.Println("Client started")
+
+	// Todo
+}
