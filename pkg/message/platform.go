78f48d2646ed19605b46f62a682e580126027919
Lucas <lgrati@fi.uba.ar>
2024-10-07 00:14:45 -0300

    platform counter worker

--- pkg/message/platform.go
+++ pkg/message/platform.go
@@ -9,3 +9,18 @@ type Platform struct {
 func (p Platform) ToBytes() ([]byte, error) {
 	return toBytes(p)
 }
+
+func (p Platform) IsEmpty() bool {
+	return p.Windows == 0 && p.Linux == 0 && p.Mac == 0
+}
+
+func (p *Platform) Increment(other Platform) {
+	p.Windows += other.Windows
+	p.Linux += other.Linux
+	p.Mac += other.Mac
+}
+
+func PlatfromFromBytes(b []byte) (Platform, error) {
+	var m Platform
+	return m, fromBytes(b, &m)
+}
