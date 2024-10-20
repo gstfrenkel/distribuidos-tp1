a120ea71bb9a86ac383e618c2262c90399eddc89
valenadels <v.adelsflugel@despegar.com>
2024-10-20 15:46:16 -0300

    topN handles many clients2

--- internal/worker/top_n/top_n_test.go
+++ internal/worker/top_n/top_n_test.go
@@ -8,62 +8,68 @@ import (
 
 func TestPublishNewGame(t *testing.T) {
 	f := fakeFilter(5)
-
+	clientId := "0-0"
 	msg := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 10}}
-	f.updateTop(msg)
-	if f.top.Len() != 1 {
-		t.Errorf("Expected top length 1, got %d", f.top.Len())
+	f.updateTop(msg, clientId)
+	priorityQueue := f.top[clientId]
+	if priorityQueue.Len() != 1 {
+		t.Errorf("Expected top length 1, got %d", priorityQueue.Len())
 	}
-	if f.top[0].GameId != 1 || f.top[0].Votes != 10 {
-		t.Errorf("Expected GameId 1 with 10 Votes, got GameId %d with %d Votes", f.top[0].GameId, f.top[0].Votes)
+	if priorityQueue[0].GameId != 1 || priorityQueue[0].Votes != 10 {
+		t.Errorf("Expected GameId 1 with 10 Votes, got GameId %d with %d Votes", priorityQueue[0].GameId, priorityQueue[0].Votes)
 	}
 }
 
 func TestPublishExistingGame(t *testing.T) {
 	f := fakeFilter(5)
+	clientId := "0-0"
 	msg1 := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 10}}
 	msg2 := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 15}}
-	f.updateTop(msg1)
-	f.updateTop(msg2)
-	if f.top.Len() != 1 {
-		t.Errorf("Expected top length 1, got %d", f.top.Len())
+	f.updateTop(msg1, clientId)
+	f.updateTop(msg2, clientId)
+	priorityQueue := f.top[clientId]
+	if priorityQueue.Len() != 1 {
+		t.Errorf("Expected top length 1, got %d", priorityQueue.Len())
 	}
-	if f.top[0].GameId != 1 || f.top[0].Votes != 15 {
-		t.Errorf("Expected GameId 1 with 15 Votes, got GameId %d with %d Votes", f.top[0].GameId, f.top[0].Votes)
+	if priorityQueue[0].GameId != 1 || priorityQueue[0].Votes != 15 {
+		t.Errorf("Expected GameId 1 with 15 Votes, got GameId %d with %d Votes", priorityQueue[0].GameId, priorityQueue[0].Votes)
 	}
 }
 
 func TestPublishTopNLimit(t *testing.T) {
 	f := fakeFilter(5)
+	clientId := "0-0"
 	for i := 1; i <= 6; i++ {
 		msg := message.ScoredReviews{message.ScoredReview{GameId: int64(i), Votes: uint64(i * 10)}}
-		f.updateTop(msg)
+		f.updateTop(msg, clientId)
 	}
-	if f.top.Len() != 5 {
-		t.Errorf("Expected top length 5, got %d", f.top.Len())
+	priorityQueue := f.top[clientId]
+	if priorityQueue.Len() != 5 {
+		t.Errorf("Expected top length 5, got %d", priorityQueue.Len())
 	}
-	if heap.Pop(&f.top).(*message.ScoredReview).Votes != 20 {
-		t.Errorf("Expected min top vote 20, got %d", heap.Pop(&f.top).(*message.ScoredReview).Votes)
+	if heap.Pop(&priorityQueue).(*message.ScoredReview).Votes != 20 {
+		t.Errorf("Expected min top vote 20, got %d", heap.Pop(&priorityQueue).(*message.ScoredReview).Votes)
 	}
 }
 
 func TestInsertTwoElementsWithSameVotes(t *testing.T) {
 	f := fakeFilter(5)
-
+	clientId := "0-0"
 	msg1 := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 10}}
 	msg2 := message.ScoredReviews{message.ScoredReview{GameId: 2, Votes: 10}}
 
-	f.updateTop(msg1)
-	f.updateTop(msg2)
+	f.updateTop(msg1, clientId)
+	f.updateTop(msg2, clientId)
 
-	if f.top.Len() != 2 {
-		t.Errorf("Expected top length 2, got %d", f.top.Len())
+	priorityQueue := f.top[clientId]
+	if priorityQueue.Len() != 2 {
+		t.Errorf("Expected top length 2, got %d", priorityQueue.Len())
 	}
 
 	foundGameId1 := false
 	foundGameId2 := false
 
-	for _, item := range f.top {
+	for _, item := range priorityQueue {
 		if item.GameId == 1 && item.Votes == 10 {
 			foundGameId1 = true
 		}
@@ -82,14 +88,15 @@ func TestInsertTwoElementsWithSameVotes(t *testing.T) {
 
 func TestTopToTopNSlice(t *testing.T) {
 	f := fakeFilter(2)
+	clientId := "0-0"
 	msg := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 10}}
-	f.updateTop(msg)
+	f.updateTop(msg, clientId)
 	msg = message.ScoredReviews{message.ScoredReview{GameId: 2, Votes: 20}}
-	f.updateTop(msg)
+	f.updateTop(msg, clientId)
 	msg = message.ScoredReviews{message.ScoredReview{GameId: 3, Votes: 30}}
-	f.updateTop(msg)
+	f.updateTop(msg, clientId)
 
-	top := f.getTopNScoredReviews()
+	top := f.getTopNScoredReviews(clientId)
 	if len(top) != 2 {
 		t.Errorf("Expected top length 2, got %d", len(top))
 	}
@@ -98,6 +105,41 @@ func TestTopToTopNSlice(t *testing.T) {
 	}
 }
 
+func TestManyClientTops(t *testing.T) {
+	f := fakeFilter(2)
+	clientId1 := "0-0"
+	clientId2 := "0-1"
+	msg := message.ScoredReviews{message.ScoredReview{GameId: 1, Votes: 10}}
+	f.updateTop(msg, clientId1)
+	msg = message.ScoredReviews{message.ScoredReview{GameId: 2, Votes: 20}}
+	f.updateTop(msg, clientId1)
+	msg = message.ScoredReviews{message.ScoredReview{GameId: 3, Votes: 30}}
+	f.updateTop(msg, clientId1)
+
+	msg = message.ScoredReviews{message.ScoredReview{GameId: 4, Votes: 40}}
+	f.updateTop(msg, clientId2)
+	msg = message.ScoredReviews{message.ScoredReview{GameId: 5, Votes: 50}}
+	f.updateTop(msg, clientId2)
+	msg = message.ScoredReviews{message.ScoredReview{GameId: 6, Votes: 60}}
+	f.updateTop(msg, clientId2)
+
+	top1 := f.getTopNScoredReviews(clientId1)
+	if len(top1) != 2 {
+		t.Errorf("Expected top1 length 2, got %d", len(top1))
+	}
+	if top1[0].GameId != 3 || top1[0].Votes != 30 {
+		t.Errorf("Expected top1 GameId 3 with 30 Votes, got GameId %d with %d Votes", top1[0].GameId, top1[0].Votes)
+	}
+
+	top2 := f.getTopNScoredReviews(clientId2)
+	if len(top2) != 2 {
+		t.Errorf("Expected top2 length 2, got %d", len(top2))
+	}
+	if top2[0].GameId != 6 || top2[0].Votes != 60 {
+		t.Errorf("Expected top2 GameId 6 with 60 Votes, got GameId %d with %d Votes", top2[0].GameId, top2[0].Votes)
+	}
+}
+
 func fakeFilter(n int) *filter {
-	return &filter{top: make(PriorityQueue, 0, n), n: n}
+	return &filter{top: make(map[string]PriorityQueue), n: n}
 }
