da9243562dade3485494f775867f4088b9419f3f
Gastón Frenkel <gst.frenkel@gmail.com>
2024-11-26 23:45:48 -0300

    fixes to origin IDs and EOF sending

--- internal/worker/platform_counter/platform_counter.go
+++ internal/worker/platform_counter/platform_counter.go
@@ -42,6 +42,8 @@ func (f *filter) Process(delivery amqp.Delivery, headers amqp.Header) ([]sequenc
 	}
 	clientCounter := f.counters[headers.ClientId]
 
+	headers = headers.WithOriginId(amqp.Query1originId) // TODO: Add sequence ID
+
 	switch headers.MessageId {
 	case message.EofMsg:
 		logs.Logger.Infof("Received EOF for client %s! Sending platform count: %v", headers.ClientId, clientCounter)
@@ -53,7 +55,6 @@ func (f *filter) Process(delivery amqp.Delivery, headers amqp.Header) ([]sequenc
 		if err != nil {
 			logs.Logger.Errorf("%s: %s", errors.FailedToPublish.Error(), err)
 		}
-
 	case message.PlatformID:
 		msg, err := message.PlatfromFromBytes(delivery.Body)
 		if err != nil {
@@ -83,8 +84,7 @@ func (f *filter) publish(headers amqp.Header) {
 
 	logs.Logger.Infof("Sending %v for client %s with key %s", *platforms, headers.ClientId, f.w.Outputs[0].Key)
 
-	headers = headers.WithMessageId(message.PlatformID).WithOriginId(amqp.Query1originId) // TODO: Add sequence ID
-
+	headers = headers.WithMessageId(message.PlatformID)
 	if err = f.w.Broker.Publish(f.w.Outputs[0].Exchange, f.w.Outputs[0].Key, b, headers); err != nil {
 		logs.Logger.Errorf("%s: %s", errors.FailedToPublish.Error(), err)
 	}
