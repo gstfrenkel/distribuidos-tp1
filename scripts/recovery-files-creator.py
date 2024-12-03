fb61f132300e70f33d8a0589db603ade82328173
valenadels <v.adelsflugel@despegar.com>
2024-12-03 16:31:03 -0300

    fix script

--- scripts/recovery-files-creator.py
+++ scripts/recovery-files-creator.py
@@ -5,10 +5,10 @@ with open('../docker-compose.yaml', 'r') as file:
     docker_compose_content = file.read()
 
 os.makedirs('../volumes', exist_ok=True)
-volume_pattern = re.compile(r'- \../volumes/[^:]+') # regex to match volumes in the form of - ./volumes/...
+volume_pattern = re.compile(r'- \./volumes/[^:]+') # regex to match volumes in the form of - ./volumes/...
 volumes = volume_pattern.findall(docker_compose_content)
 
 for volume in volumes:
     host_path = volume.split(':')[0].strip('- ')
-    with open(host_path, 'w') as f:
+    with open( '.' + host_path, 'w') as f:
         pass
