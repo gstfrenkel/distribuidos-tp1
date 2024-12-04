93fc4de74144aa0d9c336d62077c949929fe3fab
valenadels <v.adelsflugel@despegar.com>
2024-12-03 22:31:41 -0300

    verify hc

--- scripts/random-kill.sh
+++ scripts/random-kill.sh
@@ -16,12 +16,21 @@ list_containers() {
 main() {
     KILL_INTERVAL=10
     KILL_COUNT=5
+
     while true; do
         containers=($(list_containers))
-
+        n_hc=0
         if [ ${#containers[@]} -gt 0 ]; then
             for ((i=0; i<KILL_COUNT && i<${#containers[@]}; i++)); do
                             container_to_kill=${containers[$RANDOM % ${#containers[@]}]}
+
+                            if [[ "$container_to_kill" =~ healthchecker_.* ]]; then
+                                n_hc=$n_hc + 1
+                                if [ $n_hc -gt $KILL_COUNT-1 ]; then
+                                    n_hc=0
+                                    continue
+                                fi
+                            fi
                             docker kill "$container_to_kill" &>/dev/null
                             echo "Container $container_to_kill has been killed."
                         done
