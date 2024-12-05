14707ed1dead678f95b7ad5914c742ff1e6ccca1
Lucas <lgrati@fi.uba.ar>
2024-12-05 14:15:40 -0300

    update generate compose script

--- scripts/generate-docker-compose.sh
+++ scripts/generate-docker-compose.sh
@@ -8,10 +8,9 @@ read -p "Enter the number of platform filter instances: " platform_count
 read -p "Enter the number of joiner counter filter instances: " joiner_counter_count
 read -p "Enter the number of joiner top filter instances: " joiner_top_count
 read -p "Enter the number of joiner percentile filter instances: " joiner_percentile_count
-read -p "Enter the number of percentile filter instances: " percentile_count
 read -p "Enter the number of topn filter instances: " topn_count
 read -p "Enter the number of platform counter instances: " platform_counter_count
 read -p "Enter the number of topn playtime filter instances: " topn_playtime_count
 read -p "Enter the number of release date filter instances: " release_date_count
 
-python3 generate_docker_compose.py "$reviews_count" "$review_text_count" "$action_count" "$indie_count" "$platform_count" "$joiner_counter_count" "$joiner_top_count" "$joiner_percentile_count" "$percentile_count" "$topn_count" "$platform_counter_count" "$topn_playtime_count" "$release_date_count"
+python3 generate_docker_compose.py "$reviews_count" "$review_text_count" "$action_count" "$indie_count" "$platform_count" "$joiner_counter_count" "$joiner_top_count" "$joiner_percentile_count" "$topn_count" "$platform_counter_count" "$topn_playtime_count" "$release_date_count"
